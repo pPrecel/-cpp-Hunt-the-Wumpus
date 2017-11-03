86687a0939d53efed34fd72d31c6ef144a1ba237
Filip Strózik <filip.strozik@outlook.com>
2017-11-03 01:18:40 +0100

    +"map generator" bugs fixed, +"find" method upgraded, +map structure changed, +map.png upgraded.

--- HuntTheHumpus/main.cpp
+++ HuntTheHumpus/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 namespace map {
 	enum RoomState {
 		nothing = 0,
 		monster = 1,
-		catapulta = 2,
+		catapult = 2,
 		player = 3,
 		hole = 4
 	};
 
 	class Room {
+	private:
 		int number;
 		Room* adjoing_1;
 		Room* adjoing_2;
@@ -20,24 +23,25 @@ namespace map {
 		Room(const int& n, const RoomState& rs = nothing, Room* r1 = nullptr, Room* r2 = nullptr, Room* r3 = nullptr)
 			: number{ n }, state{ rs }, adjoing_1{ r1 }, adjoing_2{ r2 }, adjoing_3{ r3 } {}
 
-
-
 		Room* find(int i) {
 			Room* tmp = this;
+			while(tmp->number != i) tmp = closer_room(i, tmp->adjoing_1, tmp->adjoing_2, tmp->adjoing_3);
+			return tmp;
+		}
 
-			for (int j = 0; j != 30; j--) {
-				if (tmp->number == i) return tmp;
-
-				if (j > 8 && i > tmp->number && tmp->number % 2 == 1) tmp = tmp->adjoing_3;
-				else if (j > 8 && i < tmp->number && tmp->number % 2 == 0) tmp = tmp->adjoing_3;
-				else tmp = tmp->adjoing_2;
-			}
-			return this;
+		Room* find_in_circle(int number) {
+			Room* tmp = this;
+			do {
+				tmp = tmp->adjoing_1;
+			} while (tmp->number != number && tmp->number != this->number);
+			return (tmp->number == number) ? tmp : nullptr;
 		}
 
 		friend Room* map_generator();
 		friend Room* create_circle(int start, int end);
 		friend void merge_circles(Room* firCircle, Room* secCircle, Room* thiCircle);
+		friend Room* closer_room(int target, Room* firstRoom, Room* secondRoom, Room* thirdRoom);
+		friend void random_states(Room* room, int howMany, RoomState state);
 	};
 
 	Room* create_circle(int start, int end) {
@@ -79,15 +83,61 @@ namespace map {
 		Room* secCircle = create_circle(6, 15);
 		Room* thiCircle = create_circle(16, 20);
 
+		secCircle = secCircle->find_in_circle(8);
+		thiCircle = thiCircle->find_in_circle(18);
+
 		merge_circles(firCircle, secCircle, thiCircle);
+
+		random_states(firCircle, 4, catapult);
+		random_states(firCircle, 4, hole);
+		random_states(firCircle, 1, monster);
+		random_states(firCircle, 1, player);
 
 		return firCircle;
 	}
+
+	void random_states(Room* room, int howMany, RoomState newState) {
+		srand(time(NULL));
+
+		for (int i = 0; i != howMany; i++) {
+			int random;
+			do{ 
+				random = (rand() % 20) +1; 
+				room = room->find(random);
+			} while (room->state != 0);
+			room->state = newState;
+		}
+		return;
+	}
+
+	int how_far(int first, int second) {
+		return abs(first - second);
+	}
+
+	Room* closer_room(int target, Room* firstRoom, Room* secondRoom, Room* thirdRoom) {
+		Room* tmp = (how_far(target, firstRoom->number) <= how_far(target, secondRoom->number)) ? firstRoom : secondRoom;
+		return (how_far(target, tmp->number) <= how_far(target, thirdRoom->number)) ? tmp : thirdRoom;
+
+	}
 }
 
+namespace game_engine {
+
+}
 
 int main() {
 	map::Room* map = map::map_generator();
 
 	return 0;
 }
+
+/*Room* find(int i) {
+			Room* tmp = this;
+
+			for (int j = 0; tmp->number == i; j--) {
+				if (j > 8 && i > tmp->number && tmp->number % 2 == 1) tmp = tmp->adjoing_3;
+				else if (j > 8 && i < tmp->number && tmp->number % 2 == 0) tmp = tmp->adjoing_3;
+				else tmp = tmp->adjoing_2;
+			}
+			return tmp;
+			}*/
