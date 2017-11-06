e5cee2521e2ed76c84285d4bdc8fe23ccff01de0
Filip Strózik <filip.strozik@outlook.com>
2017-11-06 23:22:33 +0100

    +upgrade "fill_container", +upgrade "check_string",

--- HuntTheHumpus/main.cpp
+++ HuntTheHumpus/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 namespace map {
 	enum RoomState {
@@ -41,7 +42,7 @@ namespace map {
 		friend Room* create_circle(int start, int end);
 		friend void merge_circles(Room* firCircle, Room* secCircle, Room* thiCircle);
 		friend Room* closer_room(int target, Room* firstRoom, Room* secondRoom, Room* thirdRoom);
-		friend void random_states(Room* room, int howMany, RoomState state);
+		friend Room* random_states(Room* room, int howMany, RoomState state);
 	};
 
 	Room* create_circle(int start, int end) {
@@ -88,15 +89,10 @@ namespace map {
 
 		merge_circles(firCircle, secCircle, thiCircle);
 
-		random_states(firCircle, 4, catapult);
-		random_states(firCircle, 4, hole);
-		random_states(firCircle, 1, monster);
-		random_states(firCircle, 1, player);
-
 		return firCircle;
 	}
 
-	void random_states(Room* room, int howMany, RoomState newState) {
+	Room* random_states(Room* room, int howMany, RoomState newState) {
 		srand(time(NULL));
 
 		for (int i = 0; i != howMany; i++) {
@@ -107,7 +103,7 @@ namespace map {
 			} while (room->state != 0);
 			room->state = newState;
 		}
-		return;
+		return room;
 	}
 
 	int how_far(int first, int second) {
@@ -119,25 +115,192 @@ namespace map {
 		return (how_far(target, tmp->number) <= how_far(target, thirdRoom->number)) ? tmp : thirdRoom;
 
 	}
+
 }
 
 namespace game_engine {
+	struct CommandContainer {
+		char step;
+		int firRoom;
+		int secRoom;
+		int thiRoom;
+	};
 
+	void greeting() {
+		std::cout << "Witaj poszukiwaczu!" << std::endl;
+		std::cout << "Polowanie na Humpusa zawiodlo Cie do ciemnej pieczary potwora." << std::endl;
+		std::cout << "Niestety, spadaj¹c z urwiska, usiad³eœ na strza³ach i pochodni, przez co, w totalnej, majac jedynie 5 strzal, musisz upolowac Humpusa." << std::endl;
+		std::cout << "Kazda strzala moze przeleciec przez trzy komnaty. Wystarczy jeden celny strzal, by ubic bestie! Sptrzelanie wykonuje sie poprzez podanie litery s,"
+			<< " oraz trzech komnat, oddzielonych myslnikami." << std::endl;
+		std::cout << "Pamietaj jednak, ze kazdorazowy strzal sprawia, ze potwor przechodzi do losowej komory, znajdujacej sie obok." << std::endl;
+		std::cout << "Wejscie do komnaty, w ktorej znajduje sie potwor, skutuje smiercia. Tak samo dzieje sie, gdy potwor wejdzie do komnaty z graczej." << std::endl;
+		std::cout << "Na planszy rozstawiona jest tez pewna ilosc katapult i komnat bez podlogi." << std::endl;
+		std::cout << "Wejscie do pomoju bez katapulty skutkuje natychmiastowym zakonczeniem gry, natomiast wejscie na katapulte powoduje przerzucenie do losowego pokoju." << std::endl;
+		std::cout << "Postacia kierujemy poprzez wpisanie literki m oraz podanie sasiedniego pokoju. Istnieje tez opcja podpowiedzi, czyli tak zwanego trybu debug."
+			<< " Pokazuje ona komnaty, w ktorych znajduja sie przeszkody i gracz.";
+		std::cout << "Przyklady koment: " << std::endl << "ruch -> m13" << std::endl << "strzal -> s13-14-15" << std::endl << "debug -> d" << std::endl << std::endl << std::endl;
+	}
+
+	//toNumber(text[n], text[n+1]);
+	int toNumber(char fir, char sec = 0) {
+		return (sec) ? (int(sec - 48) * 10 + int(fir - 48)) : int(fir - 48);
+	}
+
+	bool check_string(std::string& text) {
+		if (text[0] != 'd' && text[0] != 'm' && text[0] != 's') return false;
+
+		if (text[0] == 'm') {
+			if (text.length() < 2 || text.length() > 3) return false;
+
+			if (text[1] < 48 || text[1] > 57) return false;
+			else if(text[2] != '\0' && (text[2] < 48 || text[2] > 57)) return false;
+			return true;
+		}		
+		else if (text[0] == 's') {
+			if (text.length() < 6 || text.length() > 9) return false;
+
+			int howManyDecimals = 0;
+			int howManyDashes = 0;
+
+			for (int i = 1; i < text.length(); i++) {
+				if (text[i] >= 48 && text[i] <= 57) howManyDecimals += 1;
+				else if (text[i] == '-') {
+					howManyDashes += 1;
+					howManyDecimals = 0;
+				}
+				else return false;
+				if (howManyDashes > 2 || howManyDecimals > 3) return false;
+			}
+		}
+		else if (text[0] == 'd' && text.length() == 1) return true;
+		else return false;
+
+		return true;
+	}
+
+	bool fill_Container(CommandContainer& container, std::string& text) {
+		if (!check_string(text)) return false;
+		char move = text[0];
+
+		if (move == 'd') {
+			container.step = move;
+			container.firRoom = NULL;
+			container.secRoom = NULL;
+			container.thiRoom = NULL;
+			return true;
+		}
+
+		if (move == 'm') {
+			int number;
+			if (text[2] == '\0' || text[2] == '\n') {
+				if (text[1] < 48 || text[1] > 57) return false;
+				number = toNumber(text[1]);
+			}
+			else if (text[3] == '\0' || text[3] == '\n') {
+				if (text[1] < 48 || text[1] > 57 || text[2] < 48 || text[2] > 57) return false;
+				number = toNumber(text[2], text[1]);
+			}
+			else return false;
+
+			if (number < 0 || number > 20) return false;
+
+			container.step = move;
+			container.firRoom = number;
+			container.secRoom = NULL;
+			container.thiRoom = NULL;
+			return true;
+		}
+		
+		if (move == 's') {	
+			int lastDash;
+			int firRoom, secRoom, thiRoom;
+
+			if (text[2] == '-') {
+				lastDash = 2;
+				firRoom = toNumber(text[1]);
+			}
+			else {
+				lastDash = 3;
+				firRoom = toNumber(text[2], text[1]);
+			}
+
+			if (text[lastDash + 2] == '-') {
+				secRoom = toNumber(text[lastDash + 1]);
+				lastDash += 2;
+			}
+			else {
+				secRoom = toNumber(text[lastDash + 2], text[lastDash + 1]);
+				lastDash += 3;
+			}
+
+			if (text[lastDash + 2] == '\0' || text[lastDash + 2] == '\n') {
+				thiRoom = toNumber(text[lastDash + 1]);
+			}
+			else {
+				thiRoom = toNumber(text[lastDash + 2], text[lastDash + 1]);
+			}
+
+			if (firRoom < 0 || firRoom > 20) return false;
+			if (secRoom < 0 || secRoom > 20 || secRoom == firRoom) return false;
+			if (thiRoom < 0 || thiRoom > 20 || thiRoom == firRoom || thiRoom == secRoom) return false;
+
+			container.step = move;
+			container.firRoom = firRoom;
+			container.secRoom = secRoom;
+			container.thiRoom = thiRoom;
+		}
+
+		return true;
+	}
+
+	bool execute_move(CommandContainer& container) {
+		return true;
+	}
+
+	CommandContainer get_move() {
+		std::string nextMove;
+		CommandContainer tmp;
+		bool flag = true;
+		do {
+			if (!flag) std::cout << "Zle wprowadzone dane" << std::endl;
+
+			std::cout << "Co chcesz robic?" << std::endl;
+			std::cin >> nextMove;
+			flag = fill_Container(tmp, nextMove);
+			
+			//pkt testowy
+			std::cout << "tmp.step: " << tmp.step << " tmp.firRoom: " << tmp.firRoom << " tmp.secRoom: " << tmp.secRoom << " tmp.thiRoom : " << tmp.thiRoom << std::endl;
+		} while (!flag);
+		return tmp;
+	}
+
+	bool main_engine() {
+		greeting();
+
+		map::Room* map = map::map_generator();
+		map::Room* monsterPos = map::random_states(map, 1, map::monster);
+		map::Room* playerPos = map::random_states(map, 1, map::player);
+		map::random_states(map, 4, map::catapult);
+		map::random_states(map, 4, map::hole);
+		
+		CommandContainer commands;
+
+		
+		bool endingFlag = 0;
+		std::string nextMove = "";
+		do {
+			commands = get_move();
+			execute_move(commands);
+
+			std::cout << "STOP" << std::endl << std::endl;
+		} while (!endingFlag);
+
+		return true;
+	}
 }
 
 int main() {
-	map::Room* map = map::map_generator();
-
+	
+	while (game_engine::main_engine());
 	return 0;
 }
-
-/*Room* find(int i) {
-			Room* tmp = this;
-
-			for (int j = 0; tmp->number == i; j--) {
-				if (j > 8 && i > tmp->number && tmp->number % 2 == 1) tmp = tmp->adjoing_3;
-				else if (j > 8 && i < tmp->number && tmp->number % 2 == 0) tmp = tmp->adjoing_3;
-				else tmp = tmp->adjoing_2;
-			}
-			return tmp;
-			}*/
