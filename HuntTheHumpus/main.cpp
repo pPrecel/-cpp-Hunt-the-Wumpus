fcc648f21b1c9098da6f263f665449b84852feb0
Filip Strózik <filip.strozik@outlook.com>
2017-11-08 21:31:15 +0100

    +added player movment, +added player's shot and monster's reaction for that, +added debugger mode, +bug fixed,
    -removed test methods.

--- HuntTheHumpus/main.cpp
+++ HuntTheHumpus/main.cpp
@@ -38,11 +38,145 @@ namespace map {
 			return (tmp->number == number) ? tmp : nullptr;
 		}
 
+		void print_debugger() {
+			Room* map = this;
+			std::cout << "          Map" << std::endl;
+			std::cout << "       ----1----" << std::endl;
+			std::cout << "      /    |    \\" << std::endl;
+			std::cout << "     /   --8--   \\" << std::endl;
+			std::cout << "   _/   /     \\   \\_" << std::endl;
+			std::cout << "  /    7        9   \\" << std::endl;
+			std::cout << " /    / \\      / \\   \\" << std::endl;
+			std::cout << " 5---6   17--18   10--2" << std::endl;
+			std::cout << " |   |   /    \\    |  |" << std::endl;
+			std::cout << " |   15--16   19--11  |" << std::endl;
+			std::cout << " |   |   \\    /    |  |" << std::endl;
+			std::cout << " \\   \\     20     /   /" << std::endl;
+			std::cout << "  \\   14    |   12   /" << std::endl;
+			std::cout << "   \\ /  \\--13---/ \\ /" << std::endl;
+			std::cout << "    4--------------3" << std::endl << std::endl;
+
+			for (int i = 1; i <= 20; i++) {
+				std::cout << "Room " << i << ": ";
+				map = map->find(i);
+				if (map->state == 0) std::cout << "nothing" << std::endl;
+				else if(map->state == 1) std::cout << "monster" << std::endl;
+				else if (map->state == 2) std::cout << "catapult" << std::endl;
+				else if (map->state == 3) std::cout << "player" << std::endl;
+				else if (map->state == 4) std::cout << "hole" << std::endl;
+			}
+		}
+
+		Room* move_player(int to) {
+			if (this->adjoing_1->number != to && this->adjoing_2->number != to && this->adjoing_3->number != to) return this;
+
+			Room* nextRoom = this->find(to);
+			
+			if (nextRoom->state == nothing) {
+
+				if(this->state != catapult)
+					this->state = nothing;
+				nextRoom->state = player;
+
+				std::cout << "Trafiles do komory nr: " << nextRoom->number << ". Zlo jest coraz blizej." << std::endl;
+
+				nextRoom->show_dangerous();
+
+				return nextRoom;
+			}
+			else if (nextRoom->state == catapult) {
+				srand(time(NULL));
+
+				if (this->state != catapult)
+					this->state = nothing;
+
+				int way = (rand() % 3) + 1;
+
+				if (way == 1) return nextRoom->move_player(nextRoom->adjoing_1->number);
+				if (way == 2) return nextRoom->move_player(nextRoom->adjoing_2->number);
+				if (way == 3) return nextRoom->move_player(nextRoom->adjoing_3->number);
+			}
+			else if (nextRoom->state == monster) return nullptr;
+			else if (nextRoom->state == hole) return nullptr;
+			return this;
+		}
+
+		void show_dangerous() {
+			if (this->adjoing_1->state == monster || this->adjoing_2->state == monster || this->adjoing_3->state == monster)
+				std::cout << "	Cos tu smierdzi zgnilym miesem. Potwor jest blisko!" << std::endl;
+			if (this->adjoing_1->state == hole || this->adjoing_2->state == hole || this->adjoing_3->state == hole)
+				std::cout << "	Brrrr... Ale tu zimno, czuje przeciag!" << std::endl;
+			if (this->adjoing_1->state == catapult || this->adjoing_2->state == catapult || this->adjoing_3->state == catapult)
+				std::cout << "	Hmmm? Co to za odglos? Chyba gdzies blisko jest jakiej mechaniczne ustrojstwo!" << std::endl;
+		}
+
+		Room* move_monster() {
+			Room* monsterPos = this;
+
+			srand(time(NULL));
+
+			int flag = 20;
+
+			do{
+				int way = (rand() % 3) + 1;
+				if (way == 1 && (monsterPos->adjoing_1->state == nothing || monsterPos->adjoing_1->state == player)) {
+					if (monsterPos->adjoing_1->state == player) return nullptr;
+					monsterPos->state = nothing;
+					monsterPos->adjoing_1->state = monster;
+					return monsterPos->adjoing_1;
+				}
+				if (way == 2 && (monsterPos->adjoing_2->state == nothing || monsterPos->adjoing_2->state == player)) {
+					if (monsterPos->adjoing_2->state == player) return nullptr;
+					monsterPos->state = nothing;
+					monsterPos->adjoing_2->state = monster;
+					return monsterPos->adjoing_2;
+				}
+				if (way == 3 && (monsterPos->adjoing_3->state == nothing || monsterPos->adjoing_3->state == player)) {
+					if (monsterPos->adjoing_3->state == player) return nullptr;
+					monsterPos->state = nothing;
+					monsterPos->adjoing_3->state = monster;
+					return monsterPos->adjoing_3;
+				}
+				flag--;
+			} while (!flag);
+			return this;
+		}
+
+		int room_number() {
+			return this->number;
+		}
+
+		bool shot(int firRoom, int secRoom, int thiRoom) {
+			Room* tmp = this;
+			int table[3] = { firRoom,secRoom,thiRoom };
+
+			for (int i = 0; i < 3; i++) {
+				if (tmp->adjoing_1 == tmp->find(table[i])) {
+					tmp = tmp->adjoing_1;
+				}
+				else if (tmp->adjoing_2 == tmp->find(table[i])) {
+					tmp = tmp->adjoing_2;
+				}
+				else if (tmp->adjoing_3 == tmp->find(table[i])) {
+					tmp = tmp->adjoing_3;
+				}
+				else return false;
+
+				if (tmp->state == monster) return true;
+			}
+			return true;
+		}
+
+		bool is_monster(int pos) {
+			return (this->find(pos)->state == monster) ? true : false;
+		}
+
 		friend Room* map_generator();
 		friend Room* create_circle(int start, int end);
 		friend void merge_circles(Room* firCircle, Room* secCircle, Room* thiCircle);
 		friend Room* closer_room(int target, Room* firstRoom, Room* secondRoom, Room* thirdRoom);
 		friend Room* random_states(Room* room, int howMany, RoomState state);
+		friend bool check_randomer(Room* room);
 	};
 
 	Room* create_circle(int start, int end) {
@@ -116,6 +250,10 @@ namespace map {
 
 	}
 
+	bool check_randomer(Room* room) {
+		if (room->adjoing_1->state == nothing || room->adjoing_2->state == nothing || room->adjoing_3->state == nothing) return true;		
+		return false;
+	}
 }
 
 namespace game_engine {
@@ -253,10 +391,6 @@ namespace game_engine {
 		return true;
 	}
 
-	bool execute_move(CommandContainer& container) {
-		return true;
-	}
-
 	CommandContainer get_move() {
 		std::string nextMove;
 		CommandContainer tmp;
@@ -267,9 +401,6 @@ namespace game_engine {
 			std::cout << "Co chcesz robic?" << std::endl;
 			std::cin >> nextMove;
 			flag = fill_Container(tmp, nextMove);
-			
-			//pkt testowy
-			std::cout << "tmp.step: " << tmp.step << " tmp.firRoom: " << tmp.firRoom << " tmp.secRoom: " << tmp.secRoom << " tmp.thiRoom : " << tmp.thiRoom << std::endl;
 		} while (!flag);
 		return tmp;
 	}
@@ -282,25 +413,57 @@ namespace game_engine {
 		map::Room* playerPos = map::random_states(map, 1, map::player);
 		map::random_states(map, 4, map::catapult);
 		map::random_states(map, 4, map::hole);
+
+		
+		int playerArrows = 5;
 		
 		CommandContainer commands;
+		bool endingFlag = false;
 
-		
-		bool endingFlag = 0;
-		std::string nextMove = "";
+		std::cout << "Zaczynasz gre w komorze nr: " << playerPos->room_number() << ". Powodzenia!" << std::endl;
+		playerPos->show_dangerous();
+
 		do {
+			//map->print_debugger();
+			std::cout << std::endl;
 			commands = get_move();
-			execute_move(commands);
 
-			std::cout << "STOP" << std::endl << std::endl;
+			if (commands.step == 'm') {
+				if (!(playerPos = playerPos->move_player(commands.firRoom))) {
+					std::cout << "Koniec gry! Przegrales!" << std::endl << std::endl << std::endl;
+					endingFlag = true;
+				}
+			}
+			else if (commands.step == 's' && playerArrows != 0) {
+				if (playerPos->shot(commands.firRoom, commands.secRoom, commands.thiRoom)) {
+					if (playerPos->is_monster(commands.firRoom) || playerPos->is_monster(commands.secRoom) || playerPos->is_monster(commands.thiRoom)){
+						std::cout << "Pokonales potwora! Gratulacje!" << std::endl << std::endl << std::endl;
+						endingFlag = true;
+					}
+					else {
+						std::cout << "Slychac swist powietrza, przecinanego strzala, ale nic poza tym. Potwor nadal zyje! Zostalo Ci " << playerArrows << " strzal." << std::endl;
+					}
+					if (!(--playerArrows)) std::cout << "O nie! Kolczan jest pusty! Teraz moge juz tylko uciekac, albo dac sie zabic!" << std::endl;
+
+					if (!(monsterPos = monsterPos->move_monster())) {
+						std::cout << "Przegrales! Potwor Cie zjadl!" << std::endl << std::endl << std::endl;
+						endingFlag = true;
+					}
+				}
+			}
+			else if (commands.step == 'd') map->print_debugger();
+
 		} while (!endingFlag);
 
+		std::cout << "W tym miejscu gra sie konczy. Ale najpierw nacisnij dowolny klawisz.";
+
+		system("PAUSE");
 		return true;
 	}
 }
 
 int main() {
 	
-	while (game_engine::main_engine());
+	game_engine::main_engine();
 	return 0;
 }
